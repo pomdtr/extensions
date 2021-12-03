bc4a4164069bfd67540feb5c11868035cef910dd
pomdtr <achille.lacoin@dailymotion.com>
2021-12-03 12:40:23 +0100

    Switch to node-localstorage

--- extensions/lazy/src/list-commands.tsx
+++ extensions/lazy/src/list-commands.tsx
@@ -1,39 +1,25 @@
 import { ActionPanel, environment, getPreferenceValues, Icon, List, PushAction } from "@raycast/api";
 import { execaSync } from "execa";
 import Frecency from "frecency";
-import { readFileSync, writeFileSync } from "fs";
 import Fuse from "fuse.js";
+import { LocalStorage } from "node-localstorage";
 import { homedir } from "os";
-import { useEffect, useMemo, useState } from "react";
+import { resolve } from "path/posix";
+import { useEffect, useState } from "react";
 import { Action, Preview, Step } from "./components";
 import { Lazy } from "./lazy";
 
 const { "lazy-path": PATH } = getPreferenceValues();
 process.env.PATH = PATH.replace("~", homedir());
 
-// SupportStorage implements the minimal API required by frecency
-class SupportStorage {
-  getItem(key: string): string | undefined {
-    try {
-      const value = readFileSync(environment.supportPath + "/" + key).toString();
-      return value;
-    } catch {
-      return undefined;
-    }
-  }
-  setItem(key: string, value: string): void {
-    writeFileSync(environment.supportPath + "/" + key, value);
-  }
-}
-
 const commandFrecency = new Frecency({
-  key: "commands.json", // "key" becomes "filename"
+  key: "commands.json",
   idAttribute: "title",
-  storageProvider: new SupportStorage(),
+  storageProvider: new LocalStorage(resolve(environment.supportPath, "frecency")),
 });
 
 export default function listCommands(): JSX.Element {
-  const [roots, setRoots] = useState<Lazy.Item[]>([]);
+  const [roots, setRoots] = useState<Lazy.Item[]>();
   const [query, setQuery] = useState<string>();
 
   useEffect(() => {
@@ -43,12 +29,12 @@ export default function listCommands(): JSX.Element {
     setRoots(roots);
   }, []);
 
-  const fuse = useMemo(() => new Fuse(roots || [], {keys: ["title", "subtitle"]}), [roots])
-
+  const fuse = new Fuse(roots || [], { keys: ["title", "subtitle"], ignoreLocation: true, threshold: 0.3 });
+  const results = query ? fuse.search(query).map((res) => res.item) : roots;
 
   const items: Lazy.Item[] = commandFrecency.sort({
     searchQuery: query,
-    results: query ? fuse.search(query).map(res => res.item): roots
+    results: results || [],
   });
 
   return (
